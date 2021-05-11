869fd07a9403fe45238f0b87a2fbf0c6d1ea3a7a
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-05-11 14:00:12 +0900

    last commit before submit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 let selectedHide = true;
 let categoryHide = true;
+let scrollChange = false;
 
 function showSelectList() {
   let selectedDownMenu = document.querySelector(
@@ -36,4 +37,82 @@ function showAllCaterory() {
       .getElementById("all-down")
       .setAttribute("class", "fas fa-caret-down");
   }
+}
+
+let num = Number(document.getElementById("buy-count").value);
+
+function addCount() {
+  num += 1;
+  document.getElementById("buy-count").value = String(num);
+  document.querySelector(".time-reward").textContent = String(102 * num);
+  document.querySelector(".before-price").textContent = String(10760 * num);
+  let realPrice = String(10160 * num);
+  document.querySelector(".major-price-time").textContent =
+    realPrice.slice(0, realPrice.length - 3) +
+    "," +
+    realPrice.slice(realPrice.length - 3, realPrice.length);
+
+  console.log(String(num));
+  console.log(String(102 * num));
+}
+
+function subCount() {
+  num -= 1;
+  document.getElementById("buy-count").value = String(num);
+  document.querySelector(".time-reward").textContent = String(102 * num);
+  document.querySelector(".before-price").textContent = String(10760 * num);
+  let realPrice = String(10160 * num);
+  document.querySelector(".major-price-time").textContent =
+    realPrice.slice(0, realPrice.length - 3) +
+    "," +
+    realPrice.slice(realPrice.length - 3, realPrice.length);
+  console.log(String(num));
+  console.log(String(102 * num));
+
+  if (num < 1) {
+    alert("0이하는 안돼요");
+    document.getElementById("buy-count").value = 1;
+    document.querySelector(".major-price-time").textContent = "10,160";
+    document.querySelector(".before-price").textContent = "10,760";
+  }
+}
+
+// 오른쪽 화살
+
+function showArrow() {
+  document.querySelector(".right-arrow").style.visibility = "visible";
+  // if (scrollChange == true) {
+  //   document.querySelector(".left-arrow").style.visibility = "visible";
+  // }
+}
+
+function hideArrow() {
+  document.querySelector(".right-arrow").style.visibility = "hidden";
+}
+
+function showRightBlue() {
+  document.querySelector(".after-arrow").style.visibility = "visible";
+  document.querySelector(".before-arrow").style.visibility = "hidden";
+}
+
+function hideRightBlue() {
+  document.querySelector(".before-arrow").style.visibility = "visible";
+  document.querySelector(".after-arrow").style.visibility = "hidden";
+}
+
+//
+
+function horizonScroll() {
+  let target = document.querySelector(".horizon-scroll");
+  let horizonScroll = target.animate(
+    [{ transform: "translateX(0)" }, { transform: "translateX(-700px)" }],
+    500
+  );
+
+  horizonScroll.addEventListener("finish", function () {
+    target.style.transform = "translateX(-700px)";
+  });
+
+  console.log(target.getBoundingClientRect().right);
+  scrollChange = true;
 }
